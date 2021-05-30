b9afe08faa4159d8872476fe3697b6f6f9abd40a
konrad <ktalaga90@gmail.com>
2021-05-30 09:55:40 +0100

    Product abstract class and Crisp, Drink, Sweet child classes created

---
+++ src/main/java/products/Crisp.java
@@ -0,0 +1,8 @@
+package products;
+
+public class Crisp extends Product{
+
+    public Crisp(String name, String brand) {
+        super(name, brand);
+    }
+}
---
+++ src/main/java/products/Sweet.java
@@ -0,0 +1,9 @@
+package products;
+
+public class Sweet extends Product{
+
+    public Sweet(String name, String brand) {
+        super(name, brand);
+    }
+
+}
